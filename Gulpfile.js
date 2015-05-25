762fdb1cd5cbbd2bf3cd54e12601a281dcd19d61
Martin Hallén <marthall@outlook.com>
2015-05-25 23:14:31 +0200

    Added watch option

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
+var paths = {
+  scripts: './lib/*.js',
+  data: './data/*.js'
+};
+
 gulp.task('scripts', function() {
-  return gulp.src(['./lib/*.js', './data/*.js'])
+  return gulp.src([paths.scripts, paths.data])
     .pipe(concat('script.js'))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+})
